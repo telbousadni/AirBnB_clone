86eb637187f2d93e8c6a3b989a0139f3b8c6976f
telbousadni <taha.elbousadni@gmail.com>
2023-08-13 15:12:00 -0700

    pystyle check

--- models/base_model.py
+++ models/base_model.py
@@ -13,31 +13,23 @@ class BaseModel():
     attributes/methods for other classes
     """
 
-    def __init__(self, *args, **kwargs):   
-    """
-    Instantation
-    """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
+    def __init__(self):
+        """
+        Instantation
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         models.storage.new(self)
-    
+
     def __str__(self):
         """
         should print: [<class name>] (<self.id>)
         <self.__dict__>
         """
-        return "[{}]({})<{}>".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}]({})<{}>".format(
+                type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -49,7 +41,7 @@ class BaseModel():
 
     def to_dict(self):
         """
-        returns a dictionary containing all 
+        returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
         dict = self.__dict__
