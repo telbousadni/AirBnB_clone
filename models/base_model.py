91da5b5fcc16a3cb6e7cda6260314692308208f1
telbousadni <taha.elbousadni@gmail.com>
2023-08-13 14:40:04 -0700

    pystyle check

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,8 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        models.storage.new(self)
     
     def __str__(self):
         """
@@ -43,6 +45,7 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
