7db7de08f8025ba232e380fb77df58ae92a586a8
telbousadni <taha.elbousadni@gmail.com>
2023-08-13 14:34:54 -0700

    pystyle check

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,19 @@ class BaseModel():
     attributes/methods for other classes
     """
 
-    def __init__(self):   
+    def __init__(self, *args, **kwargs):   
     """
     Instantation
     """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -26,7 +35,7 @@ class BaseModel():
         should print: [<class name>] (<self.id>)
         <self.__dict__>
         """
-        return "[{}]({})<{}>".format(self.__name__, self.id, self.__dict__)
+        return "[{}]({})<{}>".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
